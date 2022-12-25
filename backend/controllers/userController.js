28fbafdf324040b56009d7d6391095edd69556d0
monsorit <arifmonsorit@gmail.com>
2022-12-25 21:43:13 +0600

    LoginScreen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user & get token
-// @route   GET/api/users/login
+// @route   POST/api/users/login
 // @access   Public
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
@@ -23,6 +23,40 @@ const authUser = asyncHandler(async (req, res) => {
         throw new Error('Invalid email or password')
     }
 })
+
+
+// @desc    Register a new user
+// @route   POST/api/users
+// @access   Public
+const RegisterUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExistes = await User.findOne({ email })
+
+    if (userExistes) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+
 
 
 // @desc    Get user profile
@@ -46,4 +80,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 
 
-export { authUser, getUserProfile }
+export { authUser, RegisterUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import { authUser, RegisterUser, getUserProfile } from '../controllers/userController.js';
 const router = express.Router();
 import { protect } from '../middleware/authmiddleware.js';
 
-
+router.route('/').post(RegisterUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
